20ec5f4864bdc8795f0feaecf4fe9f28df2e5d50
sagar <spawar.sagar2@gmail.com>
2026-04-19 19:39:25 +0530

    Enhanced Playwright framework: separated E2E & functional tests, Done some changes in the code

---
+++ tests/FunctionalTest/OrderPage.js
@@ -0,0 +1,34 @@
+const { test, expect } = require("../fixtures/baseTestIns");
+const testData = require('../Utils/TestData.json');
+const { getApiPremium } = require("../Utils/apiHelper");
+
+    test("@smoke ", async ({ homePage, quotePage, proposalPage, paymentPage, contactDetailPage, orderPage, request }) => 
+    {
+      const user = testData.users[0];
+      await homePage.goTo();
+      await expect(homePage.page).toHaveURL(/travelqa/);
+      await homePage.quoteSelectionCriteria(user);
+      const quotePremium = await quotePage.quoteDetails();
+      await proposalPage.proposalDetail(user);
+      await paymentPage.paymentDetail();
+      await contactDetailPage.contactDetail(user);
+
+      const finalPremium = await orderPage.orderDetails();
+
+      // ✅ UI vs UI validation
+      expect(Math.abs(finalPremium - quotePremium)).toBeLessThanOrEqual(1);
+
+      console.log(`Quote Premium: ${quotePremium}`);
+      console.log(`Final Premium: ${finalPremium}`);
+
+      // ✅ API call (IMPORTANT: pass user + request)
+      const apiPremium = await getApiPremium(request,user);
+      console.log(`API Premium: ${apiPremium}`);
+
+      // ✅ UI vs API validation
+      expect(Math.abs(finalPremium - apiPremium)).toBeLessThanOrEqual(1);
+    });
+
+
+
+
--- tests/FunctionalTest/QuotePage.spec.js
+++ tests/FunctionalTest/QuotePage.spec.js
@@ -4,7 +4,7 @@ const testData = require("../../Utils/TestData.json");
 
 test.describe("@web @quote Quote Page Functional Test", () => {
 
-    test("@web @invalidSI Invalid Sum Insured Test", async ({ page }) => {
+    test("@invalidSI Invalid Sum Insured Test", async ({ page }) => {
 
         const homePage = new HomePage(page);
         await homePage.goTo();
@@ -43,7 +43,7 @@ test.describe("@web @quote Quote Page Functional Test", () => {
 
     });
 
-    test("@web @excluded Excluded Countries Testing ", async ({ page }) => {
+    test("@excluded Excluded Countries Testing ", async ({ page }) => {
 
         await page.goto('https://travelqa.policybazaar.com/');
 
@@ -72,19 +72,85 @@ test.describe("@web @quote Quote Page Functional Test", () => {
         await expect(plansHeader).toBeVisible();
         await expect(plansHeader).toContainText('Showing');
 
-        //Insurer Dropdown Selection
+        //Insurer Selection from Dropdown
         await page.locator('li').filter({ hasText: 'Insurer' }).click();
         const tataCheckBox = page.locator("input[value='Tata AIG']")
-
-        // Wait for checkbox
         await expect(tataCheckBox).toBeVisible(); // Assertion
         await tataCheckBox.check();
+        await page.getByRole('button', { name: 'Apply' }).click();
 
-        // Apply filter
+        // No plans found page
+        await expect(page.locator(".noQuotesCard p")).toBeVisible();
+        await expect(page.locator(".noQuotesCard p")).toContainText("No plans found");
+
+    });
+
+    test.only('@policyPdf Policy Brochure & Wording Testing', async ({ page }) => 
+    {
+        const homePage = new HomePage(page);
+        await homePage.goTo();
+        await homePage.quoteSelectionCriteria(testData.users[0]);
+
+        const plansHeader = page.locator('h2.singleProfileText');
+        await expect(plansHeader).not.toHaveText(/Loading/);
+
+        await expect(plansHeader).toBeVisible();
+        await expect(plansHeader).toContainText('Showing');
+
+        await page.locator('#premium_80068').getByText('View all features').click();
+
+        await expect(page.getByRole('link', { name: 'Plan Brochure' })).toBeVisible();
+        await page.getByRole('link', { name: 'Plan Brochure' }).click();
+        await page.locator('#policy-brochure').contentFrame().locator('.ndfHFb-c4YZDc-cYSp0e-DARUcf-PLDbbf').click();
+
+        await expect(page.getByRole('link', { name: 'Policy Wording' })).toBeVisible();
+        await page.getByRole('link', { name: 'Policy Wording' }).click();
+        await page.locator('#policy-wording').contentFrame().locator('.ndfHFb-c4YZDc-cYSp0e-DARUcf-PLDbbf').click();
+
+        await page.locator('#modal-root i').click();
+    })
+
+    test('@editOption Edit Option Testing', async ({ page }) => 
+    {
+        const homePage = new HomePage(page);
+        await homePage.goTo();
+        await homePage.quoteSelectionCriteria(testData.users[0]);
+
+        const plansHeader = page.locator('h2.singleProfileText');
+        await expect(plansHeader).not.toHaveText(/Loading/);
+
+        await expect(plansHeader).toBeVisible();
+        await expect(plansHeader).toContainText('Showing');
+
+        await page.getByText('Tata AIG').click();
+
+        await expect(page.getByRole('link', { name: 'Edit' })).toBeVisible();
+        await page.getByRole('link', { name: 'Edit' }).click();
+
+        await page.getByText('Country VisitingUSA').click();   // cross x button clicked
+
+        await page.locator('#input-row i').click();
+        await page.getByText('Enter the countries you are').click();
+        await page.getByRole('textbox', { name: 'Enter the countries you are' }).fill('iran');
+        await page.getByText('Iran').click();
+        await page.getByRole('button', { name: 'Save changes' }).click();
+
+        await page.getByText('Insurer', { exact: true }).click();
+        await page.locator('label').filter({ hasText: 'Tata AIG' }).click();
         await page.getByRole('button', { name: 'Apply' }).click();
 
         await expect(page.locator(".noQuotesCard p")).toBeVisible();
         await expect(page.locator(".noQuotesCard p")).toContainText("No plans found");
 
+        await page.getByRole('link', { name: 'Edit' }).click();
+        await page.getByText('Country VisitingIran').click();
+        await page.locator('#input-row i').click();
+        await page.getByText('Enter the countries you are').click();
+        await page.getByRole('textbox', { name: 'Enter the countries you are' }).fill('cana');
+        await page.locator('#search-country').getByText('Canada').click();
+        await page.getByRole('button', { name: 'Save changes' }).click();
+
+        await page.getByText('Tata AIG').click();
+        await page.getByRole('img', { name: 'Tata AIG' }).click();
     });
 })
